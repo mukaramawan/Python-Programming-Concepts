3ff904e4a2e7bb82a40d064a4bfdba0b0e371b86
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-08-31 01:05:50 +0500

    Path & Query Parameter, Http Exceptions

--- FastApi/main.py
+++ FastApi/main.py
@@ -2,7 +2,7 @@
 # activate the environment by running --> venv/scripts/activate
 # install fastapi uvicorn (server) pydantic (data validation library) --> pip install fastapi uvicorn pydantic
 # Start a local server for your FastAPI (or ASGI) app by running --> uvicorn main:app --reload
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -26,3 +26,32 @@ def view():
     data = load_data()
 
     return data
+
+#----------------------
+#Path & Query Parameter, Http Exceptions
+
+#path parameter
+@app.get('/patients/{patient_id}')
+def viewPatient(patient_id: str = Path(..., description="Id of the Patient", example='P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Patient not found!")
+
+#query parameter
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description="Sort on the basis of height, weight, bmi"), order: str = Query('asc', description="Sort in asc or desc order")):
+
+    valid_fields = ['height', 'weight', 'bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid Field: Select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order: select asc, desc order!')
+    
+    data = load_data()
+    sort_order = True if order=='desc' else False
+    sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+
