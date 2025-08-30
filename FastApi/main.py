78ee0becfc64a7fcac11fec21051ada4c34d2d9a
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-08-30 20:10:27 +0500

    Get Requests in FastAPI

--- FastApi/main.py
+++ FastApi/main.py
@@ -3,9 +3,26 @@
 # install fastapi uvicorn (server) pydantic (data validation library) --> pip install fastapi uvicorn pydantic
 # Start a local server for your FastAPI (or ASGI) app by running --> uvicorn main:app --reload
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+
+    return data
+
 @app.get("/")
 def Hello():
-    return {"message": "Hello World!!"}
+    return {"message": "Patients Management System!"}
+
+@app.get("/about")
+def about():
+    return {"message": "Hello! This is Eye Care Center!"}
+
+@app.get('/patients')
+def view():
+    data = load_data()
+
+    return data
