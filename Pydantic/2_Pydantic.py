cee7b02f3ede52d9135e269049ef9e3bcef7c093
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-08-31 15:59:55 +0500

    Data Type Validations in Pydantic

--- Pydantic/2_Pydantic.py
+++ Pydantic/2_Pydantic.py
@@ -1,19 +1,26 @@
 # Install Pydantic by --> pip install pydantic
 
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 class patient(BaseModel):
     name: str
     age: int
+    weight: float
+    married: bool = False
+    allergies: Optional[List[str]] = None
+    contact_details: Dict[str, str]
 
 def insert_patient(patient: patient):
     print(patient.name)
     print(patient.age)
-    print("inserted!")
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')
 
-patient1 = {'name': 'Mukaram Awan', 'age': 18}
+patient_info = {'name':'Mukaram Awan', 'age': '18', 'weight': 75.2,'married': True, 'contact_details':{'phone':'123456789'}}
 
-patient1 = patient(**patient1)
+patient1 = patient(**patient_info)
 
 insert_patient(patient1)
 
