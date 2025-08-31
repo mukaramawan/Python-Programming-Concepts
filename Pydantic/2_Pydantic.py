dc37aaa18e98efc0bd525a901b67d380195b91f5
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-09-01 00:29:11 +0500

    Custom Data Validations & Annotations

--- Pydantic/2_Pydantic.py
+++ Pydantic/2_Pydantic.py
@@ -1,12 +1,16 @@
 # Install Pydantic by --> pip install pydantic
 
-from pydantic import BaseModel
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class patient(BaseModel):
-    name: str
+    #name: str = Field(max_length=20)
+    name: Annotated[str, Field(max_length=20, title='Name of Patient', description='Enter the name of the patient less than 20 chars!', examples=['Mukaram Awan', 'Muhammad Mukaram'])]
     age: int
-    weight: float
+    email: EmailStr
+    linkedin: AnyUrl
+    # weight: float = Field(gt=0)
+    weight: Annotated[float, Field(gt=0, strict=True)]  #now can't handle '75.2'
     married: bool = False
     allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
@@ -18,7 +22,7 @@ def insert_patient(patient: patient):
     print(patient.married)
     print('updated')
 
-patient_info = {'name':'Mukaram Awan', 'age': '18', 'weight': 75.2,'married': True, 'contact_details':{'phone':'123456789'}}
+patient_info = {'name':'Mukaram Awan', 'age': '18', 'email': 'mukaram@gmail.com','linkedin': 'https://www.linkedin.com/in/mukaram-awan/', 'weight': 75.2,'married': True, 'contact_details':{'phone':'123456789'}}
 
 patient1 = patient(**patient_info)
 
