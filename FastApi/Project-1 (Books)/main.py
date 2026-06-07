8aa80b0a6e684f75ace14f69ecf6f28709d2c8b5
Muhammad Mukarram Awan <141765944+mukaramawan@users.noreply.github.com>
2026-06-07 12:34:55 +0500

    Validation with pydantic

--- FastApi/Project-1 (Books)/main.py
+++ FastApi/Project-1 (Books)/main.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field, computed_field
 
 app = FastAPI()
 
@@ -16,6 +19,24 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="The ID is not needed on the creation", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=5 ,max_length=200)
+    rating: int = Field(gt=0, le=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "The Great Gatsby",
+                "author": "F. Scott Fitzgerald",
+                "description": "A novel set in the Roaring Twenties that explores themes of wealth, love, and the American Dream.",
+                "rating": 5
+            }
+        }
+    }
+
 books = [
     Book(1, "The Great Gatsby", "F. Scott Fitzgerald", "A novel set in the Roaring Twenties that explores themes of wealth, love, and the American Dream.", 5),
     Book(2, "To Kill a Mockingbird", "Harper Lee", "A novel that addresses racial injustice and moral growth in the Deep South during the 1930s.", 5),
@@ -23,10 +44,15 @@ books = [
 ]
 
 @app.get("/books")
-async def get_books():
+async def get_books(): 
     return books 
-
+  
 @app.post("/create_book")
-async def create_book(book = Body()):
-    books.append(book)
+# async def create_book(book = Body()):
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    books.append(find_book_id(new_book))
+
+def find_book_id(book: Book):
+    book.id = 1 if len(books) == 0 else books[-1].id + 1
     return book
